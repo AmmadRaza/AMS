ae957c1949fb1dbe01cb209311ef93bfe1fbf3f9
AmmadRaza <ammadraza001@gmail.com>
2017-08-14 16:16:26 -0700

    Commit

---
+++ AMS/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace AMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        TaskEntities db = new TaskEntities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            TaskEntities db = new TaskEntities();
+            var list = db.TblStudents.ToList();
+            ViewBag.record = list;
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult List(TblAttendance am, string Date, int[] StudentId)
+        {
+
+            for (int i = 0; i < StudentId.Length; i++)
+            {
+                am.S_Id = StudentId[i];
+
+                db.TblAttendances.Add(am);
+                db.SaveChanges();
+            }
+
+            return Content("Done");
+        } 
+    }
+}
---
+++ AMS/Models/Atten.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public class Atten
+    {
+        public int Id { get; set; }
+
+        public string Date { get; set; }
+
+        public bool isSelected { get; set; }
+    }
+}
